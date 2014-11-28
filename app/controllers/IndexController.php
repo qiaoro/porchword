d6a5ee071d8bb6965c39136c9bfcd7df250ebbb1
刘巧如 <1123807380@qq.com>
2014-11-28 16:19:15 +0800

    ssss

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,12 +5,9 @@ class IndexController extends BaseController
 
     public function indexAction()
     {
-        echo phpinfo();
-        $this ->view ->disable();
         $this ->log('goto index');
     }
     public function personAction(){
-    	$this ->view->start()->render('index', 'person')->finish();
         $this ->log('-goto person-');
     }
 
