da50188bbc3a3c17e8554116a2ae157e07101547
刘巧如 <1123807380@qq.com>
2014-11-28 14:22:28 +0800

    sdds

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,8 +5,14 @@ class IndexController extends BaseController
 
     public function indexAction()
     {
-        echo "<h1>Hello!</h1>";
         echo phpinfo();
+        $this ->logger ->log("这是log打印的消息,默认级别是debug");
+        $this ->logger ->log('info', "这是log打印的消息,我是info");
+        $this ->logger ->debug("这是debug级别的消息");
+        $this ->logger ->info("这是info级别的消息");
+        $this ->logger ->warning("这是warning级别的消息");
+        $this ->logger ->error("这是error级别的消息");
+        $this ->log('我是封装的日志方法');
     }
 
 }
